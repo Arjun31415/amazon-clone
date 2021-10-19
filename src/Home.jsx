f4d2873e4f2196d1aa2ca0ef9c9a86eebbf608be
Arjun31415 <arjunp0710@gmail.com>
2021-10-19 21:18:34 +0530

    removed uuid from products for better optimization

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,32 +15,32 @@ function Home() {
 				/>
 				<div className="home__row">
 					<Product
-						key={uuidv4()}
-						id={uuidv4()}
+						key={"12345600"}
+						id={"12345600"}
 						title="Casio G-Shock Analog-Digital Red Dial Men's Watch-GBA-400-4ADR (G559)"
 						price={6996.0}
 						image="https://m.media-amazon.com/images/I/71mv-swdxsL._UX522_.jpg"
 						rating={4.5}
 					/>
 					<Product
-						key={uuidv4()}
-						id={uuidv4()}
+						key={"12345601"}
+						id={"12345601"}
 						title="boAt Rockerz 550"
 						price={1499}
 						image="https://m.media-amazon.com/images/I/61F5SXdi9jL._SX522_.jpg"
 						rating={4}
 					/>
 					<Product
-						key={uuidv4()}
-						id={uuidv4()}
+						key={"12345602"}
+						id={"12345602"}
 						title="10.or Crafted for Amazon Cosmos Smartwatch"
 						image="https://m.media-amazon.com/images/I/51i302mhUBL._SX522_.jpg"
 						price={1799}
 						rating={3.5}
 					/>
 					<Product
-						key={uuidv4()}
-						id={uuidv4()}
+						key={"12345603"}
+						id={"12345603"}
 						title="Hyper X Cloud Stinger Core"
 						image="https://m.media-amazon.com/images/I/512qH+i8PLS._SX522_.jpg"
 						price={2790}
@@ -49,24 +49,24 @@ function Home() {
 				</div>
 				<div className="home__row">
 					<Product
-						key={uuidv4()}
-						id={uuidv4()}
+						key={"12345604"}
+						id={"12345604"}
 						title="Hyper X Cloud Stinger Core"
 						image="https://m.media-amazon.com/images/I/61bVcEoMx3L._SX522_.jpg"
 						price={2790}
 						rating={4}
 					/>
 					<Product
-						key={uuidv4()}
-						id={uuidv4()}
+						key={"12345605"}
+						id={"12345605"}
 						title="Hyper X Cloud Stinger Core"
 						image="https://m.media-amazon.com/images/I/61bVcEoMx3L._SX522_.jpg"
 						price={2790}
 						rating={4}
 					/>
 					<Product
-						key={uuidv4()}
-						id={uuidv4()}
+						key={"12345606"}
+						id={"12345606"}
 						title="Hyper X Cloud Stinger Core"
 						image="https://m.media-amazon.com/images/I/61bVcEoMx3L._SX522_.jpg"
 						price={2790}
@@ -75,8 +75,8 @@ function Home() {
 				</div>
 				<div className="home__row">
 					<Product
-						key={uuidv4()}
-						id={uuidv4()}
+						key={"12345607"}
+						id={"12345607"}
 						title="Samsung 34-inch (86.40cm) Curved Monitor- 21:9 Ultrawide QLED, Thunderbolt 3 Port- LC34J791WTWXXL, Gray "
 						image="https://m.media-amazon.com/images/I/91pi34PiUPL._SX522_.jpg"
 						price={25999}
