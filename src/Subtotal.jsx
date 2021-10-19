e94c6104e65fa96adcc4bf8809ea2235c5c2cdd8
Arjun31415 <arjunp0710@gmail.com>
2021-10-19 19:37:51 +0530

    Dynamically update Basket price on checkout page

--- src/Subtotal.jsx
+++ src/Subtotal.jsx
@@ -2,11 +2,17 @@ import "./Subtotal.css";
 
 import CurrencyFormat from "react-currency-format";
 import React from "react";
+import { useStateValue } from "./StateProvider";
 
 function getBasketTotal(basket) {
-	return 0;
+	let price = 0;
+	for (let item of basket) {
+		price += item.price;
+	}
+	return price;
 }
-function Subtotal({ basket }) {
+function Subtotal() {
+	const [{ basket }] = useStateValue();
 	return (
 		<div className="subtotal">
 			<CurrencyFormat
