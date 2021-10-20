1c693e9749f7f5bba179c383bf79380eacba6270
Arjun31415 <arjunp0710@gmail.com>
2021-10-20 09:34:28 +0530

    Use Firebase for user auth

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,58 @@
 import "./Login.css";
 
 import { Link, useHistory } from "react-router-dom";
+import React, { useState } from "react";
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from "@firebase/auth";
 
-import React from "react";
+import { auth } from "./firebase";
 
 export default function Login(props) {
+	const history = useHistory();
+	function signIn(e) {
+		e.preventDefault();
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				console.log(userCredential);
+				const user = userCredential.user;
+				if (userCredential) {
+					history.push("/");
+				}
+				// ...
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				console.error({ code: errorCode, message: errorMessage });
+			});
+	}
+	function signUp(e) {
+		e.preventDefault();
+		createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				console.log(userCredential);
+				const user = userCredential.user;
+				if (userCredential) {
+					history.push("/");
+				}
+				// ...
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				alert(errorMessage);
+				console.error({ code: errorCode, message: errorMessage });
+				// ..
+			});
+	}
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
 	return (
 		<div className="login">
 			<Link to="/">
@@ -18,11 +66,23 @@ export default function Login(props) {
 				<h1>{"Sign In"}</h1>
 				<form>
 					<h5>Email</h5>
-					<input type="text" className="" />
+					<input
+						type="text"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
 
 					<h5>Password</h5>
-					<input type="password" className="" />
-					<button type="submit" className="login__siginInBtn">
+					<input
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<button
+						type="submit"
+						className="login__siginInBtn"
+						onClick={signIn}
+					>
 						Sign In
 					</button>
 				</form>
@@ -31,7 +91,9 @@ export default function Login(props) {
 					and Privacy Notice.
 				</p>
 				<p> New to AMAZON-CLONE? </p>
-				<button className="login__signUpBtn">Create Account</button>
+				<button className="login__signUpBtn" onClick={signUp}>
+					Create Account
+				</button>
 			</div>
 		</div>
 	);
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,11 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import firebase, { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 const firebaseConfig = {
 	apiKey: "AIzaSyDXPt15SJP0yqmfxIGmICz2QpxpNbgzCQ8",
 	authDomain: "clone-5943a.firebaseapp.com",
@@ -8,3 +15,8 @@ const firebaseConfig = {
 	appId: "1:233487007814:web:420e0fe544e4d8880c0a47",
 	measurementId: "G-P01WSGRTQ0",
 };
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+export { db, auth };
